749bac047200412f9c71fc903e753d1223173a3f
Daniel C <50636323+floranova13@users.noreply.github.com>
2022-04-22 17:29:53 -0400

    Put pool into seperate file

--- index.js
+++ index.js
@@ -8,13 +8,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+
+
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
@@ -22,27 +17,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-
 app.use('/api', routes);
 
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM blightsources');
-    const results = { 'results': result ? result.rows : null };
-
-    res.send(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send('Error ' + err);
-  }
-});
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback)
-  },
-}
--- src/routes/blightsources.js
+++ src/routes/blightsources.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { query } = require('../../');
+const { query } = require('../db');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
