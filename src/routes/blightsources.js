5967154f24fade08385d3257dfbe437b9057ddcf
Daniel C <50636323+floranova13@users.noreply.github.com>
2022-04-23 03:53:25 -0400

    Update blightsources query

--- src/routes/blightsources.js
+++ src/routes/blightsources.js
@@ -3,7 +3,7 @@ const { query } = require('../db');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const sql = 'SELECT b.id, b.name, c.name, s.name FROM blightsources b, subcategories s, categories c WHERE b.subcategory_id = s.id AND b.category_id = c.id;';
+  const sql = 'SELECT b.id, b.name, c.name AS category, s.name AS subcategory FROM blightsources b, subcategories s, categories c WHERE b.subcategory_id = s.id AND b.category_id = c.id;';
   const { rows } = await query(sql);
 
   // const blightsourcesArr = rows.map(row => {
