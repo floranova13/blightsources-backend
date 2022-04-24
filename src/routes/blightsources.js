852a5ab8bbec32bb4a47c4a73a15a2d94fabdbac
Daniel C <50636323+floranova13@users.noreply.github.com>
2022-04-23 21:26:02 -0400

    Update blightsources routes

--- src/routes/blightsources.js
+++ src/routes/blightsources.js
@@ -3,28 +3,34 @@ const { query } = require('../db');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const sql = 'SELECT b.id, b.name, c.name AS category, s.name AS subcategory FROM blightsources b, subcategories s, categories c WHERE b.subcategory_id = s.id AND b.category_id = c.id;';
+  const sql =
+    'SELECT b.id, b.name, c.name AS category, s.name AS subcategory, b.description, b.rarity FROM blightsources b, subcategories s, categories c WHERE b.subcategory_id = s.id AND b.category_id = c.id;';
   const { rows } = await query(sql);
 
-  // const blightsourcesArr = rows.map(row => {
-  //   const { name } = row;
-  // })
+  const blightsourcesArr = rows.map((row) => {
+    const { name, category, subcategory, description, rarity } = row;
+  });
 
-  res.send(rows);
+  res.send(blightsourcesArr);
 });
 
 router.get('/:name', async (req, res) => {
-  const { name } = req.params;
-  const sql = 'SELECT * FROM blightsources WHERE blightsources.name = $1;';
-  const { rows } = await query(sql, [name]);
-  res.send(rows);
+  const { name: blightsourceName } = req.params;
+  const sql =
+    'SELECT b.id, b.name, c.name AS category, s.name AS subcategory, b.description, b.rarity FROM blightsources b, subcategories s, categories c WHERE b.subcategory_id = s.id AND b.category_id = c.id AND b.name = $1;';
+  const { rows } = await query(sql, [blightsourceName]);
+
+  const { name, category, subcategory, description, rarity } = rows[0];
+
+  res.send({ name, category, subcategory, description, rarity });
 });
 
-router.post('/', async (req, res) => {
-  const { name } = req.body;
-  const sql = 'INSERT INTO blightsources (name) VALUES ($1);';
-  const data = await query(sql, [name]);
-  res.send(data);
-});
+// // IT WILL CURRENTLY ERROR OUT ON A POST
+// router.post('/', async (req, res) => {
+//   const { name } = req.body;
+//   const sql = 'INSERT INTO blightsources (name) VALUES ($1);';
+//   const data = await query(sql, [name]);
+//   res.send(data);
+// });
 
 module.exports = router;
