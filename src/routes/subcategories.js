66e0fdedd1d6ed7c00def48aa2c9e1daa99b37f7
Daniel C <50636323+floranova13@users.noreply.github.com>
2022-04-26 22:30:17 -0400

    Update subcategories post route

--- src/routes/subcategories.js
+++ src/routes/subcategories.js
@@ -22,12 +22,11 @@ router.get('/:name', async (req, res) => {
   res.send({ name, category, description });
 });
 
-// POST WILL ERROR OUT
-// router.post('/', async (req, res) => { // eventually add the ability to input a description
-//   const { name, category_name } = req.body;
-//   const sql = 'INSERT INTO subcategories (name, category_name) VALUES ($1, (SELECT id from categories c WHERE c.name = $2));';
-//   const data = await query(sql, [name, category_name]);
-//   res.send(data);
-// });
+router.post('/', async (req, res) => { // eventually add the ability to input a description
+  const { name, category_name } = req.body;
+  const sql = 'INSERT INTO subcategories (name, category_id) SELECT $1, c.id FROM categories c WHERE c.name = $2;';
+  const data = await query(sql, [name, category_name]);
+  res.send(data);
+});
 
 module.exports = router;
